a261e898d7e4b3397d1c123f940f5b5a25d55f27
sidesh01 <siddeshtex63@gmail.com>
2024-01-14 08:41:51 +0530

    to do app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
-function App() {
+const App = () => {
+  const [todos] = useState([
+    { id: 1, text: 'Learn React' },
+    { id: 2, text: 'Build a To-Do App' },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo App</h1>
+      <TodoList todos={todos} />
     </div>
   );
 }
